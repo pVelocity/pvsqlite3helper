1dac5e5922fe850f251425333039429ff039dc8b
Allen Chung <allen.chung@pvelocity.com>
2021-12-08 17:51:33 -0500

    changed postInstall to only copy bindings for parent node_modules folder

--- postinstall.js
+++ postinstall.js
@@ -9,10 +9,12 @@ var fs = require('fs-extra');
 var path = require('path');
 
 function main() {
+  if (path.basename(path.resolve(__dirname, '..')) === 'node_modules') {
     fs.copySync(path.resolve(__dirname, './sqlite3'), path.resolve(__dirname, '../sqlite3'), {
-        overwrite: false
+      overwrite: false
     });
-    return;
+  }
+  return;
 }
 
 main();
